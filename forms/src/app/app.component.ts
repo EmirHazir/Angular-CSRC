e4d0e8cae50bc7e39eab57368f90eaa5a91f3164
EmirHazir <emircanaziz@gmail.com>
2017-04-06 20:14:28 +0300

    forms conti...

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from "@angular/forms";
 
 @Component({
@@ -7,10 +7,25 @@ import { NgForm } from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signUpForm: NgForm;
+  defaultQuestion = 'teacher';
+  answer = '';
+  genders = ['male', 'female'];
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signUpForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
-  onSubmit(form:NgForm) {
-    console.log(form)
+
+  onSubmit() {
+    console.log(this.signUpForm)
   }
+
+  // onSubmit(form:NgForm) {
+  //   console.log(form)
+  // }
 }   
