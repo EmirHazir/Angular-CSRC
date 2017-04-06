fffa33c31d667a11c6fe23d914ade1d76d7e7725
EmirHazir <emircanaziz@gmail.com>
2017-04-06 16:21:44 +0300

    yo

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
-  onSubmit() {
+  onSubmit(form:NgForm) {
+    console.log(form)
   }
 }   
