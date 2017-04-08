80fd6a0043762576c38cc41fdc876ecd5b7c6448
EmirHazir <emircanaziz@gmail.com>
2017-04-08 22:01:45 +0300

    forms-reactive

---
+++ forms-reactive/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signupForm : FormGroup;
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male')
+    });   
+  }
+
+  onSubmit(){
+    console.log(this.signupForm);
+  }
+}
